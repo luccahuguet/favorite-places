480194a3ed09213e40beb3205d16e8af615613d1
luccahuguet <lucca.huguet2@gmail.com>
2024-06-17 22:23:47 -0300

    iphone ok(?)

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,8 +1,32 @@
 import { Button, View } from "react-native";
-import { launchCameraAsync } from "expo-image-picker";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
 
 function ImagePicker() {
+  const [permission, askForPermission] = useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (permission.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await askForPermission();
+      return permissionResponse.granted;
+    }
+    if (permission.status === PermissionStatus.DENIED) {
+      alert("You need to grant camera permissions to use this app.");
+      return false;
+    }
+    return true;
+  }
+
   async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
